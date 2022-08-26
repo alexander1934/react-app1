7aa0194b219044d41ac88cf9cbf9fd1b989e6952
Alexander Kovalenko <alexankovalenko23@gmail.com>
2022-08-26 17:12:45 +0300

    added Header.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        React приложение
-      </h1>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>Menu</li>
+      <li>Options</li>
+    </ul>
+  );
+};
+
+export default Header;
