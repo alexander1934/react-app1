40fd2673a9be78a8e9bb65c99f72046cf702c9f9
Alexander Kovalenko <alexankovalenko23@gmail.com>
2022-08-26 17:37:49 +0300

    added Content.js

---
+++ src/Content.js
@@ -0,0 +1,17 @@
+import "./App.css"
+
+const Content = () => {
+  return (
+    <div>
+      <div className="sidebar">
+        <div>My profile</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Games</div>
+        <div>Settings</div>
+      </div>
+    </div>
+  );
+};
+
+export default Content
