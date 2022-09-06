13c61a674177aabbd926e0ae0a317d6bd7f40b33
Alexander Kovalenko <alexankovalenko23@gmail.com>
2022-09-06 18:33:18 +0300

    Work on style

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import header from "./Header.module.css";
 
 const Header = () => {
   return (
-  <div className={header.block}>Social Network</div>
+  <div className={header.block}><div className={header.content}>Social Network</div></div>
   );
 };
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,11 +3,11 @@ import sidebar from "./Sidebar.module.css";
 const Sidebar = () => {
   return (
     <div className={sidebar.list}>
-      <div>My profile</div>
-      <div>News</div>
-      <div>Music</div>
-      <div>Games</div>
-      <div>Settings</div>
+      <div className={sidebar.item}>My profile</div>
+      <div className={sidebar.item}>News</div>
+      <div className={sidebar.item}>Music</div>
+      <div className={sidebar.item}>Games</div>
+      <div className={sidebar.item}>Settings</div>
     </div>
   );
 };
