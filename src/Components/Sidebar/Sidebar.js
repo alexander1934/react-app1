b20dda45abe4d3ce60c648e14cc69c9d13135790
Alexander Kovalenko <alexankovalenko23@gmail.com>
2022-09-21 10:30:16 +0300

    Added URL links

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -2,13 +2,13 @@ import sidebar from "./Sidebar.module.css";
 
 const Sidebar = () => {
   return (
-    <div className={sidebar.list}>
-      <div className={sidebar.item}>My profile</div>
-      <div className={sidebar.item}>News</div>
-      <div className={sidebar.item}>Music</div>
-      <div className={sidebar.item}>Games</div>
-      <div className={sidebar.item}>Settings</div>
-    </div>
+    <ul className={sidebar.list}>
+      <li className={sidebar.item}><a href="/profile">My profile</a></li>
+      <li className={sidebar.item}><a href="/messages">Messages</a></li>
+      <li className={sidebar.item}><a href="/news">News</a></li>
+      <li className={sidebar.item}><a href="/music">Music</a></li>
+      <li className={sidebar.item}><a href="/settings">Settings</a></li>
+    </ul>
   );
 };
 
